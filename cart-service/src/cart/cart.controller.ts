0297519efa70d60355b161b659a13390c9f27e1b
Francisco Carvalho <84443911+inclyner@users.noreply.github.com>
2025-10-04 10:15:49 +0100

    swagger docs

--- cart-service/src/cart/cart.controller.ts
+++ cart-service/src/cart/cart.controller.ts
@@ -1,25 +1,33 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CartService } from './cart.service';
-import { CreateCartDto } from './dto/create-cart.dto';
-import { UpdateCartDto } from './dto/update-cart.dto';
 import { UserId } from 'src/auth/user-id.decorator';
 import { AddItemDto } from './dto/add-item.dto';
 import { UpdateQtyDto } from './dto/update-qty.dto';
+import { ApiBadRequestResponse, ApiHeader, ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('cart')
+@ApiSecurity('x-user-id')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
-
+  @ApiHeader({ name: 'x-user-id', required: true, description: 'User UUID' })
+  @ApiOkResponse({ description: 'Return the current user cart' })
+  @ApiBadRequestResponse({ description: 'Missing x-user-id' })
   @Get('me')
   getMine(@UserId() userId: string) {
     return this.cartService.getMine(userId);
   }
 
+  @ApiOkResponse({ description: 'Add or increase an item' })
+  @ApiHeader({ name: 'x-user-id', required: true, description: 'User UUID' })
   @Post('items')
   addItem(@UserId() userId: string, @Body() dto: AddItemDto) {
     return this.cartService.addItem(userId, dto);
   }
 
+  @ApiHeader({ name: 'x-user-id', required: true, description: 'User UUID' })
+  @ApiOkResponse({ description: 'Set item quantity' })
   @Patch('items/:productId')
   setQty(
     @UserId() userId: string,
@@ -29,11 +37,15 @@ export class CartController {
     return this.cartService.setItemQuantity(userId, productId, dto.quantity);
   }
 
+  @ApiHeader({ name: 'x-user-id', required: true, description: 'User UUID' })
+  @ApiOkResponse({ description: 'Remove one item' })
   @Delete('items/:productId')
   removeItem(@UserId() userId: string, @Param('productId') productId: string) {
     return this.cartService.removeItem(userId, productId);
   }
 
+  @ApiHeader({ name: 'x-user-id', required: true, description: 'User UUID' })
+  @ApiOkResponse({ description: 'Clear all items' })
   @Delete('items')
   clear(@UserId() userId: string) {
     return this.cartService.clear(userId);
--- cart-service/src/cart/dto/update-qty.dto.ts
+++ cart-service/src/cart/dto/update-qty.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsInt, Min } from 'class-validator';
 export class UpdateQtyDto {
+  @ApiProperty({ minimum: 1, example: 3 })
+  @Type(() => Number)
   @IsInt() @Min(1) quantity: number;
 }
